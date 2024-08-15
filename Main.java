750ee5fe51d5a605bb9be5d1ebedf3124bb18fd2
shahdyousef66 <131907455+shahdyousef66@users.noreply.github.com>
2024-08-15 11:39:51 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,20 +3,10 @@ import java.util.Date;
 import java.util.*;
 
 public class Main {
-    public Shahd getIdInfo(int nationalID){
-     return new Shahd(nationalID);
-    }
     public List printUserInfo(UserInfo uI){
-
-
-
      return List.of(uI.getName(),uI.getUserName(),uI.getId());
     }
     public static void main(String[] args) {
-        Main main=new Main();
-        System.out.println(main.getIdInfo(2000496898));
-
-
         UserInfo uI=new UserInfo(123,"ahmad","ahmad123");
         System.out.println(main.printUserInfo(uI));
 
